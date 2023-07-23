3ad0dda4ba3571145e20b4128875f425dd570c8d
Mustafa Bebek <mmustafab324@gmail.com>
2023-07-23 11:20:24 +0300

    regulation

--- Coding Exercises/Basic Data Structures and Objects/find_root.py
+++ Coding Exercises/Basic Data Structures and Objects/find_root.py
@@ -7,5 +7,5 @@ delta = b ** 2 - 4 * a * c
 x1 = (-b - delta ** 0.5) / (2 * a)
 x2 = (-b + delta ** 0.5) / (2 * a)
 
-print("Birinci Kök: {}\nİkinci Kök: {}\n".format(x1,x2))
+print("First Root: {}\nSecond Root: {}\n".format(x1,x2))
 
